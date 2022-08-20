b81a3b7f361a2dc539109630afeebbcbd9414ea8
Hermione Allognon <allognonhermione@gmail.com>
2022-08-20 15:44:27 +0200

    feat: made algo le nombre le plus grand

--- 5.PlusGrandNbTableau/AlgoDebut/app.js
+++ 5.PlusGrandNbTableau/AlgoDebut/app.js
@@ -1,21 +1,48 @@
 function plusGrandNb(arr) {
 
+  let plusGrand = []
+  for (let i = 0; i < arr.length; i++) {
+    let max = 0;
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] > max) {
+        max = arr[i][j]
+      }
+    }
+    plusGrand.push(max)
+  }
+
+  return plusGrand
 }
+// function plusGrandNb(arr) {
+//   let maximum = [];
+//   arr.forEach((item) => {
+//     let result = item.reduce((accumulateur, valeurCourante) => {
+//       if (valeurCourante > accumulateur) {
+//         return valeurCourante
+//       } else {
+//         return accumulateur
+//       }
+
+//     }, 0)
+//     maximum.push(result)
+//   })
+//   return maximum
+// }
 
 
 console.log(plusGrandNb([
-                        [1, 5, 8, 3],
-                        [15, 47, 88, 26],
-                        [32, 35, 37, 39],
-                        [3000, 1001, 857, 1]
-                      ]));
+  [1, 5, 8, 3],
+  [15, 47, 88, 26],
+  [32, 35, 37, 39],
+  [3000, 1001, 857, 1]
+]));
 
 
 
 // ----------------- ÉNONCÉ -----------------
 
-// Créez un Algorithme qui prends un tableau composé de plusieurs tableaux en 
-// argument et qui retourne un seul tableau qui contient la valeur maximale de 
+// Créez un Algorithme qui prends un tableau composé de plusieurs tableaux en
+// argument et qui retourne un seul tableau qui contient la valeur maximale de
 // chaque tableaux.
 
 // ----------------- CONSEILS -----------------
